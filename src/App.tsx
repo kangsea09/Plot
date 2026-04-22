f09874ee0ddf26a1caee37bb1c73f7d07fc6364f
강세아 <gangsea817@gmail.com>
2026-04-22 12:18:01 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import "./index.css";
+
+function App() {
+  return (
+    <Routes>
+      <Route />
+    </Routes>
+  );
+}
+
+export default App;
