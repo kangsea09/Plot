1ab75d77af52d7069a5b0d4b80204a9d3f08afe9
강세아 <gangsea817@gmail.com>
2026-05-27 14:12:40 +0900

    feat: 페이지별 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-import "./index.css";
 import MainPage from "./pages/MainPage";
+import HistoryPage from "./pages/HistoryPage";
+import RankingPage from "./pages/RankingPage";
+import AiGuidePage from "./pages/AiGuidePage";
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-    </Routes>
-  );
-}
+const App = () => (
+  <Routes>
+    <Route path="/" element={<MainPage />} />
+    <Route path="/history" element={<HistoryPage />} />
+    <Route path="/ranking" element={<RankingPage />} />
+    <Route path="/ai-guide" element={<AiGuidePage />} />
+  </Routes>
+);
 
 export default App;
