439722e1e038970fb798a24fdd4153f2c896a50c
강세아 <gangsea817@gmail.com>
2026-05-27 14:15:43 +0900

    fix: 레이아웃으로 코드 간편화

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,34 +1,10 @@
-import styled from "@emotion/styled";
-import Header from "../components/Header";
-import Sidebar from "../components/Sidebar";
-const MainPage = () => {
-  return (
-    <Body>
-      <Content>
-        <Sidebar />
-        <Main>
-          <Header />
-        </Main>
-      </Content>
-    </Body>
-  );
-};
+import Layout from "../components/Layout";
+import Dashboard from "../components/Dashboard";
 
-const Body = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
+const MainPage = () => (
+  <Layout>
+    <Dashboard />
+  </Layout>
+);
 
-const Content = styled.div`
-  display: flex;
-  flex: 1;
-  overflow: hidden;
-`;
-
-const Main = styled.main`
-  flex: 1;
-  overflow-y: auto;
-`;
 export default MainPage;
