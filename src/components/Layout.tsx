2562e82aa7cde62e7a402983be2d4834456f3692
강세아 <gangsea817@gmail.com>
2026-05-27 22:12:19 +0900

    feat: 레이아웃 퍼블리싱 추가

---
+++ src/components/Layout.tsx
@@ -0,0 +1,33 @@
+import styled from "@emotion/styled";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+const Layout = ({ children }: { children: React.ReactNode }) => (
+  <Body>
+    <Content>
+      <Sidebar />
+      <Main>
+        <Header />
+        {children}
+      </Main>
+    </Content>
+  </Body>
+);
+
+const Body = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+const Content = styled.div`
+  display: flex;
+  flex: 1;
+  overflow: hidden;
+`;
+const Main = styled.main`
+  flex: 1;
+  overflow-y: auto;
+`;
+
+export default Layout;
